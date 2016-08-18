cd6dbfee51a03ea8cd8e34cfea5d67e4fde5abb0
Vitaly Nikolaev <vit3011@rambler.ru>
2016-08-19 01:33:42 +0300

    back

--- Backend/src/main/Controllers/Register.kt
+++ Backend/src/main/Controllers/Register.kt
@@ -1,7 +1,24 @@
+import com.google.gson.Gson
+import com.github.salomonbrys.kotson.*
+import com.google.gson.JsonObject
 import spark.Request
 import spark.Response
 
-fun registerUser(req: Request, res: Response): String {
-    print(req.params())
-    return "Hello: " + req.params(":name")
+fun registerUser(req: Request, res: Response): JsonObject {
+    val gson = Gson()
+    try {
+        val list1 = gson.fromJson<Map<String, String>>(req.body())
+        res.status(200)
+        val obj: JsonObject = jsonObject(
+                "property" to list1["login"]
+        )
+        return obj
+    } catch (e: com.google.gson.JsonSyntaxException) {
+        res.status(400)
+        val obj: JsonObject = jsonObject(
+                "error" to "Bad JSON"
+        )
+        return obj
+    }
+
 }
--- Backend/src/main/main.kt
+++ Backend/src/main/main.kt
@@ -2,16 +2,11 @@
  * Created by nikolaev on 17.08.16.
  */
 import  spark.Spark.*
-import registerUser
+
 
 fun main(args: Array<String>) {
     port(8081)
-    val maxThreads = Runtime.getRuntime().availableProcessors()
-    threadPool(maxThreads)
 
-    get("/") { req, res -> registerUser(req, res) }
-
-
-    get("/hello/:name") { request, response -> "Hello: " + request.params(":name") }
+    post("/register") { req, res -> registerUser(req, res) }
 
 }
